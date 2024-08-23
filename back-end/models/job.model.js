76e41484ded81824ed1d90e9aef698a215e946ef
0x1Luffy <chetankesare06@gmail.com>
2024-08-23 18:22:40 +0530

    ⚡Created Company Schema.

--- back-end/models/job.model.js
+++ back-end/models/job.model.js
@@ -43,8 +43,8 @@ const jobSchema = new mongoose.Schema({
             type:mongoose.Schema.Types.ObjectId,
             ref:'Application',
         }
-    ]
+    ] 
     
-});
+},{timestamps:true});
 
 export const Job = mongoose.model("Job", jobSchema)
