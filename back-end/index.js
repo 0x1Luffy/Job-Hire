7251b9b9d717831c49aea5743cacab8aa00f04bd
0x1Luffy <chetankesare06@gmail.com>
2024-08-23 10:44:05 +0530

    ⚡Created Starter Server.

---
+++ back-end/index.js
@@ -0,0 +1,30 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import cors from 'cors';
+const app = express();
+
+app.get("/home",(req,res)=>{
+    return res.status(200).json(
+        {
+            message:"Message from server"
+        }
+    )
+})
+
+// App Middlewares 
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin: "http://localhost:5173",
+    credentials:true
+}
+
+app.use(cors(corsOptions));
+
+
+const PORT = 3000;
+app.listen(PORT, ()=>{
+    console.log(`Server Running on port ${PORT}`);
+});
