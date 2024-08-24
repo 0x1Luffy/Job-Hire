6e4313b88f120150e9bf8a6a3a531b26d1d6a4d8
0x1Luffy <chetankesare06@gmail.com>
2024-08-24 13:16:03 +0530

    ⚡Created Company Route Also Tested API's.

--- back-end/index.js
+++ back-end/index.js
@@ -5,6 +5,7 @@ const app = express();
 import dotenv from "dotenv";
 import connectDB from "./utlis//db.js";
 import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
 dotenv.config({});
 
 // App Middlewares 
@@ -25,6 +26,7 @@ const PORT = process.env.PORT || 3000;
 
 //Default REST API
 app.use("/api/v1/user",userRoute);
+app.use("/api/v1/company",companyRoute);
 
 app.listen(PORT, ()=>{
     connectDB();
