99a59c8636f345376d2394c6ae62b1b1ea6592ff
0x1Luffy <chetankesare06@gmail.com>
2024-08-24 12:27:39 +0530

    ⚡Defined Logout Route.

--- back-end/routes/user.route.js
+++ back-end/routes/user.route.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated,updateProfile);
 
 export default router;
