10c12dbbc1afa6c3ef947496c0e118a9e57a4569
0x1Luffy <chetankesare06@gmail.com>
2024-09-01 10:22:41 +0530

    🎨 Generated Footer Layout Using ChatGPT.

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
 import CategoryCarousel from './CategoryCarousel'
 import LatestJobs from './LatestJobs'
+import Footer from './Footer'
 
 const Home = () => {
   return (
@@ -11,7 +12,7 @@ const Home = () => {
       <HeroSection/>
       <CategoryCarousel/>
       <LatestJobs/>
-      {/* <Footer/> */}
+      <Footer/>
     </div>
   )
 }
